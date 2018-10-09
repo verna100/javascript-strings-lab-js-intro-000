f86565351375cb471dd25f9ab30ad5ad9c335e5c
Vernaliz Gonzalez <vernalizg@gmail.com>
2018-10-09 21:16:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello" + specialGuest + "!";
 
-var topic = "topic";
+var space= "Let's talk about space";
+
+var topic = space;
 
 var conversation = `${topic}`;
